30c902e8423e5ac4177b5f5d74fe4fb2b5283db3
Mary Bucknell <mbucknell@usgs.gov>
2016-04-08 12:32:37 -0500

    Added dependency for bootstrap-datetimepicker

--- src/test/js/karma.conf.js
+++ src/test/js/karma.conf.js
@@ -25,6 +25,7 @@ module.exports = function (config) {
 			{pattern: 'src/main/webapp/bower_components/loglevel/dist/loglevel.js', included: false},
 			{pattern: 'src/main/webapp/bower_components/backbone.stickit/backbone.stickit.js', included : false},
 			{pattern: 'src/main/webapp/bower_components/moment/moment.js', included : false},
+			{pattern: 'src/main/webapp/bower_components/eonasdan-bootstrap-datetimepicker/js/bootstrap-datetimepicker.js', included : false},
 			{pattern: 'src/main/webapp/js/*/*.js', included: false},
 			{pattern: 'src/main/webapp/js/hb_templates/*.hbs', included: false},
 			{pattern: 'src/test/js/views/*.js', included: false},

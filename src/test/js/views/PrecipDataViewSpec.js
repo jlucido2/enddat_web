ad69ba60ebadacd1e29f4c5df2d45b78cd98810e
Mary Bucknell <mbucknell@usgs.gov>
2016-04-18 08:42:32 -0500

    Added test for the context used to render the view.

--- src/test/js/views/PrecipDataViewSpec.js
+++ src/test/js/views/PrecipDataViewSpec.js
@@ -17,8 +17,8 @@ define([
 			$('body').append('<div id="test-div"></div>');
 			$testDiv = $('#test-div');
 			testModel = new Backbone.Model({
-				lat : '43.1',
-				lon : '-100.2',
+				lat : '43.1346',
+				lon : '-100.2121',
 				x : '514',
 				y : '720',
 				startDate : moment('2002-01-01', 'YYYY-MM-DD'),
@@ -28,6 +28,20 @@ define([
 
 		afterEach(function() {
 			$testDiv.remove();
+		});
+
+		it('Expects the view to be rendered with a context that contains the formatted contents of the model', function() {
+			testView = new PrecipDataView({
+				$el : $testDiv,
+				model : testModel
+			});
+			testView.render();
+			expect(testView.context.x).toEqual('514');
+			expect(testView.context.y).toEqual('720');
+			expect(testView.context.lat).toEqual('43.135');
+			expect(testView.context.lon).toEqual('-100.212');
+			expect(testView.context.startDate).toEqual('2002-01-01');
+			expect(testView.context.endDate).toEqual('2016-04-18');
 		});
 
 		it('Expects that the checkbox is not checked if the selected property is not defined in the model when the view is rendered', function() {

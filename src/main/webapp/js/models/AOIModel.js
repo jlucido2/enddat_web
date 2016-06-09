1b4b631d75ab1a8268c727226d410fd27dbdbc63
Mary Bucknell <mbucknell@usgs.gov>
2016-06-09 13:54:58 -0500

    Added tests for the new AOIModel module and added comments.

--- src/main/webapp/js/models/AOIModel.js
+++ src/main/webapp/js/models/AOIModel.js
@@ -9,14 +9,23 @@ define([
 
 	var model = Backbone.Model.extend({
 
+		/*
+		 * @return Boolean - true if the AOI should be defined using a location and radius
+		 */
 		usingProjectLocation : function() {
 			return this.has('latitude') && this.has('longitude') && this.has('radius');
 		},
 
+		/*
+		 * @return Boolean - true if the AOI should be defined by a bounding box
+		 */
 		usingAOIBox : function() {
 			return this.has('aoiBox');
 		},
 
+		/*
+		 * @return Boolean - true if the area of interest is fully defined
+		 */
 		hasValidAOI : function() {
 			var result = false;
 			if (this.usingProjectLocation()) {
@@ -28,6 +37,10 @@ define([
 			return result;
 		},
 
+		/*
+		 * @return {Object with south, west, north, east parameters} - Returns the bounding box represented by the model.
+		 *		Returns undefined if the model does not contain a fully defined area of interset.
+		 */
 		getBoundingBox : function() {
 			var result = undefined;
 			if (this.usingProjectLocation()) {

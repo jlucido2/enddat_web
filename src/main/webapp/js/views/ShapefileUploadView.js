2c8a55d9a21a65709d2c98d114ffd8f7063501aa
Yan, Andrew N <ayan@usgs.gov>
2016-06-10 11:13:53 -0500

    start handlebars template

--- src/main/webapp/js/views/ShapefileUploadView.js
+++ src/main/webapp/js/views/ShapefileUploadView.js
@@ -1,2 +1,21 @@
 /* jslint browswer: true */
 
+define([
+    'views/BaseCollapsiblePanelView',
+    'hbs!hb_templates/shpfileUpload'
+], function(BaseCollapsiblePanelView, hbTemplate){
+	"use strict";
+   var view = BaseCollapsiblePanelView.extend({
+	   template: hbTemplate,
+	   panelHeading: 'Upload a Shapefile',
+	   panelBodyId: 'upload-shapefile-panel-body',
+	   initialize: function() {
+		   alert("Init alert...")
+	   },
+	   
+	   render: function() {
+		   this.context = template;
+		   return this;
+	   }
+   });
+});

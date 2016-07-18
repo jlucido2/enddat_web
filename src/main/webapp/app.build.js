8b282fbedb6c132893280565ef91a53e1f0f57f3
Mary Bucknell <mbucknell@usgs.gov>
2016-07-18 11:20:03 -0500

    Start of an app.build.js

---
+++ src/main/webapp/app.build.js
@@ -0,0 +1,9 @@
+({
+	baseUrl: "js/",
+	paths: {
+		'handlebars': '../bower_components/handlebars/handlebars.runtime.js'
+	},
+	name : 'init',
+
+	deps : ['../bower_components/requirejs/require', '']
+});

cd3032af51202746a74bdc910047b8cbcab994ee
Yan, Andrew N <ayan@usgs.gov>
2016-07-27 13:02:24 -0500

    add new javascript libraries to app.build and karma.conf

--- src/test/js/karma.conf.js
+++ src/test/js/karma.conf.js
@@ -30,12 +30,14 @@ module.exports = function (config) {
 			{pattern: 'src/main/webapp/bower_components/backbone.stickit/backbone.stickit.js', included : false},
 			{pattern: 'src/main/webapp/bower_components/moment/moment.js', included : false},
 			{pattern: 'src/main/webapp/bower_components/eonasdan-bootstrap-datetimepicker/src/js/bootstrap-datetimepicker.js', included : false},
+			{pattern: 'src/main/webapp/bower_components/csv/lib/csv.js', included : false},
+			{pattern: 'src/main/webapp/bower_components/filesaverjs/FileSaver.js', included : false},
 			{pattern: 'src/main/webapp/js/**/*.js', included: false},
 			{pattern: 'src/main/webapp/js/hb_templates/*.hbs', included: false},
 			{pattern: 'src/test/js/views/*.js', included: false},
 			{pattern: 'src/test/js/models/*.js', included: false},
 			{pattern: 'src/test/js/utils/*.js', included: false},
-			{pattern: 'src/test/js/leafletCustomControls/*.js', included: false}
+			{pattern: 'src/test/js/leafletCustomControls/*.js', included: false},
 		],
 		// list of files to exclude
 		exclude: [
